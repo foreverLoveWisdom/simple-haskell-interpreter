726d093233220707ab3587cedebdff83fd67244b
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-12-17 01:53:31 +0700

    feat: add parsing logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,10 @@
 module Main where
 
-import Data.Char (isDigit)
+import           Data.Char (isDigit)
 
 data Expr
   = Num Int
   | Add Expr Expr
-  | Mult Expr Expr
   deriving (Show)
 
 parseExpr :: String -> Maybe Expr
@@ -14,11 +13,15 @@ parseExpr input =
     [x, "+", y] -> Just (Add (Num (read x)) (Num (read y)))
     [x]
       | all isDigit x -> Just (Num (read x))
-    _ -> Nothing
+    _ -> Nothing --
+
+eval :: Expr -> Int
+eval (Num n)     = n
+eval (Add e1 e2) = eval e1 + eval e2
 
 main :: IO ()
 main = do
   let input = "2 + 3"
   case parseExpr input of
-    Just expr -> print expr
-    Nothing -> putStrLn "Error: Invalid input" -- Handle error
+    Just expr -> print $ eval expr
+    Nothing   -> putStrLn "Error: Invalid input"
