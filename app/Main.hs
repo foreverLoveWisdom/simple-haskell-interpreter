683e05fac79ea371bf688d9c17b5a16c2f8d8cb8
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-12-17 01:39:10 +0700

    feat: add parsing logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Data.Char (isDigit)
+
+data Expr
+  = Num Int
+  | Add Expr Expr
+  | Mult Expr Expr
+  deriving (Show)
+
+parseExpr :: String -> Maybe Expr
+parseExpr input =
+  case words input of
+    [x, "+", y] -> Just (Add (Num (read x)) (Num (read y)))
+    [x]
+      | all isDigit x -> Just (Num (read x))
+    _ -> Nothing
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let input = "2 + 3"
+  case parseExpr input of
+    Just expr -> print expr
+    Nothing -> putStrLn "Error: Invalid input" -- Handle error
