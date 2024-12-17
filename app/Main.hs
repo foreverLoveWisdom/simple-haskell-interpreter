4630c7aef81b11bd0f7fe2d8332774d7130f4528
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-12-17 13:00:53 +0700

    feat: add different example for testing
    - minor format refactor for better readability

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Data.Char (isDigit, isSpace)
+import Data.Char (isDigit, isSpace)
 
 -- Define the expression data type
 data Expr
@@ -11,48 +11,58 @@ data Expr
 -- Tokenization function
 tokenize :: String -> [String]
 tokenize [] = []
-tokenize (x:xs)
+tokenize (x : xs)
   | isSpace x = tokenize xs -- Skip whitespace
   | isDigit x =
-    let (numStr, rest) = span isDigit (x : xs)
-     in numStr : tokenize rest
+      let (numStr, rest) = span isDigit (x : xs)
+       in numStr : tokenize rest
   | x `elem` "+-*/()" = [x] : tokenize xs
   | otherwise = tokenize xs -- Ignore unexpected characters
 
 -- Parsing function that takes a list of tokens and returns an Expr
 parseExpr :: [String] -> Maybe Expr
-parseExpr tokens = parseAdd tokens
+parseExpr = parseAdd
 
 -- Function to parse addition expressions
 parseAdd :: [String] -> Maybe Expr
 parseAdd tokens =
   case parseNum tokens of
-    Just (left, ("+":rest)) ->
+    Just (left, "+" : rest) ->
       case parseNum rest of
         Just (right, remainingTokens) -> Just (Add left right)
-        Nothing                       -> Nothing
+        Nothing -> Nothing
     Just (left, _) -> Just left
     Nothing -> Nothing
 
 -- Function to parse numeric literals
 parseNum :: [String] -> Maybe (Expr, [String])
-parseNum (x:xs)
+parseNum (x : xs)
   | all isDigit x = Just (Num (read x), xs)
   | otherwise = Nothing
 parseNum [] = Nothing
 
 -- Main evaluation function for the AST
 eval :: Expr -> Int
-eval (Num n)     = n
+eval (Num n) = n
 eval (Add e1 e2) = eval e1 + eval e2
 
 main :: IO ()
 main = do
   let input = "4 + 5"
   let tokens = tokenize input
+  let input2 = "0 + 11"
+  let tokens2 = tokenize input2
   putStrLn $ "Tokens: " ++ show tokens
-  case parseExpr tokens of
+  putStrLn $ "Tokens2: " ++ show tokens2
+  -- case parseExpr tokens of
+  --   Just expr -> do
+  --     putStrLn $ "Parsed Expression: " ++ show expr
+  --     print $ eval expr
+  --     putStrLn "Evaluated!"
+  --   Nothing -> putStrLn "Error: Invalid input"
+  case parseExpr tokens2 of
     Just expr -> do
       putStrLn $ "Parsed Expression: " ++ show expr
       print $ eval expr
+      putStrLn "Evaluated!"
     Nothing -> putStrLn "Error: Invalid input"
